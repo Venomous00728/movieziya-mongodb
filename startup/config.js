3ef1405a8ac2f3e6e4fe81884fec484fa433d5c4
Shoaib Khan <111381054+Venomous00728@users.noreply.github.com>
2023-12-17 16:01:22 +0500

    Update config.js

--- startup/config.js
+++ startup/config.js
@@ -1,7 +1,15 @@
-const config = require('config');
+const config = require("config");
 
-module.exports = function() {
-  if (!config.get('jwtPrivateKey')) {
-    throw new Error('FATAL ERROR: jwtPrivateKey is not defined.');
+module.exports = function () {
+  // Modify config to use environment variables directly on Vercel
+  if (process.env.NODE_ENV === "production") {
+    config.util.setModuleDefaults("custom-environment-variables", {
+      jwtPrivateKey: "VIDLY_JWT_PRIVATE_KEY", // Match Vercel env variable
+      // Other variable mappings...
+    });
   }
-}
+
+  if (!config.has("jwtPrivateKey")) {
+    throw new Error("FATAL ERROR: jwtPrivateKey is not defined.");
+  }
+};
