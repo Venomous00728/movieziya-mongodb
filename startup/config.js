ea5d303e21dca9e4cbfc38d901f9a25fe43eb686
Shoaib Khan <111381054+Venomous00728@users.noreply.github.com>
2023-12-28 23:01:38 +0500

    Update config.js

--- startup/config.js
+++ startup/config.js
@@ -1,6 +1,9 @@
 const config = require("config");
 
 module.exports = function () {
+  if (!config.get("jwtPrivateKey")) {
+    throw new Error("FATAL ERROR: jwtPrivateKey is not defined.");
+  }
   // Modify config to use environment variables directly on Vercel
   if (process.env.NODE_ENV === "production") {
     config.util.setModuleDefaults("custom-environment-variables", {
